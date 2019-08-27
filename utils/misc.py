600565f5f87b60a82cf630590496974256cfc11c
Naegin <hellonaegin@gmail.com>
2019-08-27 02:29:54 -0300

    Melhorar sistema de conversão & formatação de milisegundos para string.

--- utils/misc.py
+++ utils/misc.py
@@ -5,5 +5,15 @@ def get_length(milliseconds, letter: bool = False):
     if not milliseconds:
         return 'LIVESTREAM'
 
-    h, _ = divmod(milliseconds / 1000, 3600); m, s = divmod(_, 60)
-    return '%02dh %02dm %02ds' % (h, m, s) if letter else '%02d:%02d:%02d' % (h, m, s)
+    m, s = divmod(milliseconds / 1000, 100); h, m = divmod(m, 60)
+    if not letter:
+        return '%02d:%02d:%02d' % (h, m, s)
+
+    d, h = divmod(h, 24)
+    output = '%1ds' % s
+
+    if m > 1: output = '%1dm ' % m + output
+    if h > 1: output = '%1dh ' % h + output
+    if d > 1: output = '%1dd ' % d + output
+
+    return output

83a466d6fc212feeacca2b7f1474a25a53f8acf2
Naegin <hellonaegin@gmail.com>
2019-09-21 20:39:34 -0300

    Correção de Erros.

--- plugins/info.py
+++ plugins/info.py
@@ -48,8 +48,7 @@ class Information(commands.Cog):
 
             return await ctx.send(content=ctx.author.mention, embed=em)
 
-        prefixes = [ctx.prefix] if ctx._guild.data['options']['prefix'] \
-            else self.disco.prefixes
+        prefixes = [ctx._guild.data['options']['prefix'] or p for p in self.disco.prefixes]
 
         command = l(ctx, 'commons.command')
         prefixes = ' | '.join(f'`{prefix}<{command}>`' for prefix in prefixes)
@@ -201,8 +200,7 @@ class Information(commands.Cog):
     @commands.cooldown(1, 10, commands.BucketType.channel)
     async def _whats_my_prefix(self, ctx):
         command = l(ctx, 'commons.command')
-        prefixes = [ctx.prefix] if ctx._guild.data['options']['prefix'] \
-            else self.disco.prefixes
+        prefixes = [ctx._guild.data['options']['prefix'] or p for p in self.disco.prefixes]
 
         await ctx.send(l(ctx, 'commands.whatsmyprefix.message', {"author": ctx.author.name,
             "prefixes": ' | '.join(f'`{prefix}<{command}>`' for prefix in prefixes),
